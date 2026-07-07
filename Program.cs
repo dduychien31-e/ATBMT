01831ae93e2d42363a1342a10b543d98e26e4446
Duychien31 <doduychien310506@gmail.com>
2026-07-07 22:53:14 +0700

    Change Program.cs for .NET 8

--- Program.cs
+++ Program.cs
@@ -10,16 +10,15 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
